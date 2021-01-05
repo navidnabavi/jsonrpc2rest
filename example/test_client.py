814e3c15e343acf2c53a575c3c84e2ccd71bc4c4
Navid Nabavi <navid92@gmail.com>
2021-01-05 11:42:17 +0330

    format python test codes

--- example/test_client.py
+++ example/test_client.py
@@ -1,12 +1,21 @@
-from requests import get,post
+from requests import get, post
 
-ans = post("http://localhost:8000/json/",json={"jsonrpc":"2.0","method":"sum","params":[1,2]})
+ans = post(
+    "http://localhost:8000/json/",
+    json={"jsonrpc": "2.0", "method": "sum", "params": [1, 2]},
+)
 print(ans.text)
 
 
-ans = post("http://localhost:8000/json/",json={"jsonrpc":"2.0","method":"prod","params":[1,2]})
+ans = post(
+    "http://localhost:8000/json/",
+    json={"jsonrpc": "2.0", "method": "prod", "params": [1, 2]},
+)
 print(ans.text)
 
 
-ans = post("http://localhost:8000/json/",json={"jsonrpc":"2.0","method":"hello","params":["navid"]})
+ans = post(
+    "http://localhost:8000/json/",
+    json={"jsonrpc": "2.0", "method": "hello", "params": ["navid"]},
+)
 print(ans.text)
--- example/test_upstream.py
+++ example/test_upstream.py
@@ -1,24 +1,27 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/sum/")
 def sum():
     print("1")
-    a = request.args.get('a', '')
-    b = request.args.get('b', '')
-    print(a,b)
+    a = request.args.get("a", "")
+    b = request.args.get("b", "")
+    print(a, b)
     a = int(a)
     b = int(b)
-    return jsonify({"result":{"sum":a+b,"diff":a-b}})
+    return jsonify({"result": {"sum": a + b, "diff": a - b}})
 
 
 @app.route("/prod/")
 def prod():
     obj = request.json
-    a = obj['a']
-    b = obj['b']
-    return jsonify({"result":a+b})
+    a = obj["a"]
+    b = obj["b"]
+    return jsonify({"result": a + b})
+
 
 @app.route("/hello/<name>/")
 def hello(name):
-    return jsonify({"result":"hello {}" .format(name)})
+    return jsonify({"result": "hello {}".format(name)})
